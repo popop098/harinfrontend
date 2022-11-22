219fa674481ab539d27b593d43231820314870f5
popop098 <popop0982@naver.com>
2022-11-22 11:45:15 +0900

    .

--- pages/dashboard/[guildid].js
+++ pages/dashboard/[guildid].js
@@ -4,8 +4,8 @@ import { BackendEnpoints } from "../../tools/Constance";
 import { fetcher } from "../../tools/utils";
 import useSWR from "swr";
 import dynamic from "next/dynamic";
-import { useState } from "react";
 import { useRouter } from "next/router";
+import Image from "next/image";
 const Odometer = dynamic(import("react-odometerjs"), {
   ssr: false,
 });
@@ -19,7 +19,7 @@ export default function GuildDashboard() {
     <>
       <GuildNav />
       <div className="flex h-screen">
-        <div className="sm:block hidden">
+        <div className="sm:block hidden bg-discord-dark-sidebar">
           <Sidebar aria-label="Default sidebar example">
             <Sidebar.Items>
               <Sidebar.ItemGroup>
@@ -38,53 +38,79 @@ export default function GuildDashboard() {
             </Sidebar.Items>
           </Sidebar>
         </div>
-        <div className="p-3 h-full w-full overflow-y-auto bg-gray-500">
-          <h2 className="text-2xl font-bold">{data?.name}</h2>
-          {JSON.stringify(data)}
-          <div className="sm:flex sm:gap-2">
-            <div
-              className="px-4 py-2 bg-white rounded-lg shadow-md w-fit"
-              style={{ borderLeftWidth: "6px", borderLeftColor: "#3B82F6" }}
-            >
-              <h1 className="text-gray-500 text-md font-bold">Total Members</h1>
-              <span className="text-3xl text-black font-bold">
-                <Odometer
-                  value={!isValidating ? data.members : 0}
-                  format="(,ddd)"
-                  theme="default"
-                  duration={1500}
-                />
-              </span>
+        <div className="p-5 h-full w-full overflow-y-auto overflow-x-hidden bg-discord-dark">
+          <div className="flex items-center gap-2">
+            <Image
+              src={data?.icon}
+              width={90}
+              height={90}
+              loading="lazy"
+              className="rounded-full transform transition-all duration-150 easy-in-out hover:rounded-md"
+            />
+            <div>
+              <h2 className="text-3xl font-bold">{data?.name}</h2>
+              <p className="text-discord-white-hover">{router.query.guildid}</p>
             </div>
-            <div
-              className="px-4 py-2 bg-white rounded-lg shadow-md w-fit"
-              style={{ borderLeftWidth: "6px", borderLeftColor: "#3B82F6" }}
-            >
-              <h1 className="text-gray-500 text-md font-bold">
-                Total Channels
-              </h1>
-              <span className="text-3xl text-black font-bold">
-                <Odometer
-                  value={!isValidating ? data.channels : 0}
-                  format="(,ddd)"
-                  theme="default"
-                  duration={1500}
-                />
-              </span>
-            </div>
-            <div
-              className="px-4 py-2 bg-white rounded-lg shadow-md w-fit"
-              style={{ borderLeftWidth: "6px", borderLeftColor: "#3B82F6" }}
-            >
-              <h1 className="text-gray-500 text-md font-bold">Total Roles</h1>
-              <span className="text-3xl text-black font-bold">
-                <Odometer
-                  value={!isValidating ? data.roles : 0}
-                  format="(,ddd)"
-                  theme="default"
-                  duration={1500}
-                />
-              </span>
+          </div>
+          <div className="max-w-full mx-4 py-6 sm:mx-auto sm:px-6 lg:px-8">
+            <div className="sm:flex sm:space-x-4">
+              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow transform transition-all mb-4 w-full sm:w-1/3 sm:my-8">
+                <div className="bg-white p-5">
+                  <div className="sm:flex sm:items-start">
+                    <div className="text-center sm:mt-0 sm:ml-2 sm:text-left">
+                      <h3 className="text-sm leading-6 font-medium text-gray-400">
+                        Total Members
+                      </h3>
+                      <p className="text-3xl font-bold text-black">
+                        <Odometer
+                          value={!isValidating ? data.members : 0}
+                          format="(,ddd)"
+                          theme="default"
+                          duration={1500}
+                        />
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow transform transition-all mb-4 w-full sm:w-1/3 sm:my-8">
+                <div className="bg-white p-5">
+                  <div className="sm:flex sm:items-start">
+                    <div className="text-center sm:mt-0 sm:ml-2 sm:text-left">
+                      <h3 className="text-sm leading-6 font-medium text-gray-400">
+                        Total Channels
+                      </h3>
+                      <p className="text-3xl font-bold text-black">
+                        <Odometer
+                          value={!isValidating ? data.channels : 0}
+                          format="(,ddd)"
+                          theme="default"
+                          duration={1500}
+                        />
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow transform transition-all mb-4 w-full sm:w-1/3 sm:my-8">
+                <div className="bg-white p-5">
+                  <div className="sm:flex sm:items-start">
+                    <div className="text-center sm:mt-0 sm:ml-2 sm:text-left">
+                      <h3 className="text-sm leading-6 font-medium text-gray-400">
+                        Total Roles
+                      </h3>
+                      <p className="text-3xl font-bold text-black">
+                        <Odometer
+                          value={!isValidating ? data.roles : 0}
+                          format="(,ddd)"
+                          theme="default"
+                          duration={1500}
+                        />
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,36 @@
 /**
  * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
  */
- module.exports = {
+module.exports = {
   content: [
     "./node_modules/flowbite-react/**/*.js",
     "./pages/**/*.{ts,tsx,js,jsx}",
     "./public/**/*.html",
   ],
-  plugins: [
-    require("flowbite/plugin")
-  ],
-  theme: {},
+  plugins: [require("flowbite/plugin")],
+  theme: {
+    extend: {
+      colors: {
+        "discord-dark": "#36393f",
+        "discord-white": "#ffffff",
+        "discord-blurple": "#5865f2",
+        "discord-green": "#57f287",
+        "discord-yellow": "#f2e05d",
+        "discord-red": "#ed4245",
+        "discord-grey": "#99aab5",
+        "discord-megenta": "#EB459E",
+        "discord-dark-hover": "#3c3f44",
+        "discord-dark-active": "#43464d",
+        "discord-white-active": "#d2d5d9",
+        "discord-white-hover": "#dcdfe2",
+        "discord-white-active-text": "#060607",
+        "discord-white-hover-text": "#2f3337",
+        "discord-dark-active-text": "#edf6ff",
+        "discord-dark-hover-text": "#dcddde",
+        "discord-dark-text": "#96989d",
+        "discord-dark-sidebar": "#2f3136",
+        "discord-white-sidebar": "#f2f3f5",
+      },
+    },
+  },
 };
